8a7b253817876217ae3d1d9ed4df9a33a6b7c986
Helmi <muhamadkelmi@gmail.com>
2024-01-25 07:49:48 +0700

    Test Payment

--- app/Http/Controllers/Api/XenditController.php
+++ app/Http/Controllers/Api/XenditController.php
@@ -14,7 +14,7 @@ class XenditController extends Controller
             $header = $request->header('x-callback-token');
             if($header == env('XENDIT_CALLBACK_TOKEN')) {
                 $response = $request->all();
-                if($response) {
+                if($response['data']['hehe']) {
                     $invoice = Invoice::where('xendit_invoice_id', $response['id'])->first();
                     if(isset($invoice)) {
                         $invoice->update([
