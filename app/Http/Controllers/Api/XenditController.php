4e2346f68de5434ac1833bf05f1459d96a95fbc3
Helmi <muhamadkelmi@gmail.com>
2024-01-25 07:22:24 +0700

    Fix Callback

--- app/Http/Controllers/Api/XenditController.php
+++ app/Http/Controllers/Api/XenditController.php
@@ -45,7 +45,34 @@ class XenditController extends Controller
 
     public function handleCallBackEWallet(Request $request)
     {
-        $data = $request->all();
-        echo $data;
+        try {
+            $header = $request->header('x-callback-token');
+            if($header == env('XENDIT_CALLBACK_TOKEN')) {
+                $response = $request->all();
+                if($response['status'] == 'PAID') {
+                    $invoice = Invoice::where('xendit_invoice_id', $response['id'])->first();
+                    if($invoice) {
+                        $invoice->update([
+                            'status' => 'PAID'
+                        ]);
+                        return response()->json([
+                            'success' => 'Invoice' . $response['id'] . ' successfully paid'
+                        ], 200);
+                    } else {
+                        return response()->json([
+                            'error' => 'Invoice not found'
+                        ], 401);
+                    }
+                } else {
+                    return redirect()->route('invoice.index', ['id' => $response['id']])->with('message', 'Pembayaran sedang Pending, harap tunggu sesaat atau hubungi Admin');
+                }
+            } else {
+                return response()->json([
+                    'error' => 'Unauthorized'
+                ], 401);
+            }
+        } catch (\Exception $e) {
+
+        }
     }
 }
