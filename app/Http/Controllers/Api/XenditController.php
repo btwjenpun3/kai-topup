7ba54db944660d974a4588eb13c2c4b207ebe340
Helmi <muhamadkelmi@gmail.com>
2024-01-25 07:43:19 +0700

    Fix Payment

--- app/Http/Controllers/Api/XenditController.php
+++ app/Http/Controllers/Api/XenditController.php
@@ -50,7 +50,7 @@ class XenditController extends Controller
             if($header == env('XENDIT_CALLBACK_TOKEN')) {
                 $response = $request->all();
                 if($response['data']['status'] == "SUCCEEDED") {
-                    $invoice = Invoice::where('nomor_invoice', $response['data']['reference_id'])->first();
+                    $invoice = Invoice::where('xendit_invoice_id', $response['data']['id'])->first();
                     if(isset($invoice)) {
                         $invoice->update([
                             'status' => 'PAID'
