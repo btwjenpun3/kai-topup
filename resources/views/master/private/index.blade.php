73cbe8f153dc15a5ee55193669c3eb34dfe24fb4
Helmi <muhamadkelmi@gmail.com>
2024-02-04 09:26:27 +0700

    Update Favicon

--- resources/views/master/private/index.blade.php
+++ resources/views/master/private/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     @yield('title')
+    <link rel="icon" type="image/x-icon" href="{{ asset(Storage::url('assets/favicon.ico')) }}">
     <!-- CSS files -->
     <link href="/dist/css/tabler.min.css?1684106062" rel="stylesheet" />
     <link href="/dist/css/tabler-flags.min.css?1684106062" rel="stylesheet" />
