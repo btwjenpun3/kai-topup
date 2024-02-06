f27a8c72aa1b8e43c94aefdfc0fc7d1fb04dad18
Helmi <muhamadkelmi@gmail.com>
2024-02-06 16:00:08 +0700

    Fix

--- app/Http/Controllers/Public/TopUp/TopUpController.php
+++ app/Http/Controllers/Public/TopUp/TopUpController.php
@@ -152,7 +152,10 @@ class TopUpController extends Controller
     }
 
     public function process(Request $request)
-    {        
+    {     
+        return response()->json([                          
+            'unaccepted' => 'Payment Gateway sedang proses aktifasi'
+        ], 200);   
         try {
             $validation = $request->validate([
                 'price' => 'required',
