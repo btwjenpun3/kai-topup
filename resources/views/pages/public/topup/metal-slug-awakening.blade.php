bd93fbcb9ca9315e23768cd2f9fe323ad03062c7
Helmi <muhamadkelmi@gmail.com>
2024-02-07 18:01:55 +0700

    Update

--- resources/views/pages/public/topup/metal-slug-awakening.blade.php
+++ resources/views/pages/public/topup/metal-slug-awakening.blade.php
@@ -95,13 +95,13 @@
                                                 value="{{ $h->nama_produk }}" />
                                         </div>
                                     @endif
-                                @elseif($h->status == 3 && $h->tipe == 'Umum')
-                                    <div class="flex-column item text-center offline">
-                                        <img src="{{ asset(Storage::url($h->gambar)) }}" class="img-fluid">
-                                        <h4 class="text-md">{{ $h->nama_produk }}</h4>
-                                        <span>Rp. {{ number_format($h->harga_jual, 0, ',', '.') }}</span>
-                                    </div>
                                 @endif
+                            @elseif($h->status == 3 && $h->tipe == 'Umum')
+                                <div class="flex-column item text-center offline">
+                                    <img src="{{ asset(Storage::url($h->gambar)) }}" class="img-fluid">
+                                    <h4 class="text-md">{{ $h->nama_produk }}</h4>
+                                    <span>Rp. {{ number_format($h->harga_jual, 0, ',', '.') }}</span>
+                                </div>
                             @endif
                             @endforeach
                         </div>
