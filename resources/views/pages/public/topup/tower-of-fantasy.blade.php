58e9ddb8059a3f24dcede6b19e9c2dba7fa99c86
Helmi <muhamadkelmi@gmail.com>
2024-02-08 21:23:48 +0700

    Update

--- resources/views/pages/public/topup/tower-of-fantasy.blade.php
+++ resources/views/pages/public/topup/tower-of-fantasy.blade.php
@@ -161,8 +161,7 @@
                             <div class="row">
                                 <div class="col-md-12 col-sm-12">
                                     <div class="data-input">
-                                        <input id="userIdInput" type="text" placeholder="Masukkan Username (Tanpa @)"
-                                            required />
+                                        <input id="userIdInput" type="text" placeholder="Masukkan User ID" required />
                                     </div>
                                 </div>
                                 <div class="data-input">
