58effd56c16dde9450cda220041c4718d64902c7
Helmi <muhamadkelmi@gmail.com>
2024-02-20 18:18:28 +0700

    Update

--- resources/views/pages/public/invoice/index.blade.php
+++ resources/views/pages/public/invoice/index.blade.php
@@ -395,7 +395,7 @@
                                 </table>
                                 <div style="background-color:white;padding:5px">
                                     <img class="barcode"
-                                        src="data:image/png;base64,{{ DNS1D::getBarcodePNG($invoice->outlet->payment_code, 'C39', 3, 33) }}"
+                                        src="data:image/png;base64,{{ DNS1D::getBarcodePNG($invoice->outlet->payment_code, 'C39', 3, 55) }}"
                                         alt="barcode" />
                                 </div>
                                 <p class="text-end">Atau gunakan Barcode ini untuk di Scan oleh petugas kasir</p>
